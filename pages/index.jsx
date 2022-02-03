14c34867bf1f4e5dbb0a7b4dab7011bc93af6123
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-02-03 10:10:37 +0300

    #4 Products

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import Head from 'next/head';
+import React, { useState } from 'react';
+import ProductItem from '../components/product/ProductItem';
+import { getData } from '../utils/fetchData';
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = (props) => {
+  const [products, setProducts] = useState(props.products);
+
+  return (
+    <div className="products">
+      <Head>
+        <title>Home Page</title>
+      </Head>
+      {products.length === 0 ? (
+        <h2>No Products</h2>
+      ) : (
+        products.map((product) => <ProductItem key={product._id} product={product} />)
+      )}
+    </div>
+  );
 };
+
+export async function getServerSideProps() {
+  const res = await getData('product');
+  return {
+    props: {
+      products: res.products,
+      result: res.result,
+    },
+  };
+}
 
 export default Home;
